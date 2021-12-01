76dc139150a1efd5d8e6d075d2e5c10f8610e4c0
emerickCHALET <47389946+emerickCHALET@users.noreply.github.com>
2021-12-01 11:55:11 +0100

    Modification of required name

--- entities/user.js
+++ entities/user.js
@@ -2,7 +2,7 @@ import Joi from 'joi'
 import mongoose from 'mongoose';
 
 const UserModel = mongoose.Schema({
-    name: {type: String, required: true},
+    name: {type: String, required: false},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     userRole: {type: String, required: true},
@@ -10,7 +10,6 @@ const UserModel = mongoose.Schema({
 
 
 const schema = Joi.object({
-    name: Joi.string().min(3).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required()
 });
