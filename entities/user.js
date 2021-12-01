f86ace3c2543abd93376d80ae91549ea916cef5d
emerickCHALET <47389946+emerickCHALET@users.noreply.github.com>
2021-12-01 11:42:36 +0100

    Update Create user
    add userRole

--- entities/user.js
+++ entities/user.js
@@ -5,6 +5,7 @@ const UserModel = mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    userRole: {type: String, required: true},
 });
 
 
